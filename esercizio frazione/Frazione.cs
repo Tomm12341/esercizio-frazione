5bf8ac22cf23ea665345502539cd86d3509e00ed
Utente <Utente@DESKTOP-I8E7M3N>
2024-01-29 18:42:10 +0100

    Aggiungere i file di progetto.

---
+++ esercizio frazione/Frazione.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_frazione
+{
+    internal class Frazione
+    {
+        public Frazione(int numeratore, int denominatore)
+        {
+            Numeratore = numeratore;
+            Denominatore = denominatore;
+        }
+
+        public int Numeratore { get; set; }
+        public int Denominatore { get; set; }
+
+        private static int MCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+        public int MassimoComunDivisore(int  Numeratore, int Denominatore)
+        {
+            int mcd = Frazione.MCD(Numeratore, Denominatore);
+            Numeratore = Numeratore / mcd;
+            Denominatore = Denominatore / mcd;    
+        }
+
+        public override string ToString()
+        {
+            return $"{Numeratore}/{Denominatore}";
+        }
+
+        public static Frazione Parse(string s)
+        {
+            var parts = s.Split('/');
+            return new Frazione(int.Parse(parts[0]), int.Parse(parts[1]));
+        }
+
+    }
+}
